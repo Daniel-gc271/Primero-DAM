c42e52b15ce165392a0a60ff8f99f4c482be3296
Danielgc-271 <daniel.gonzalez.calvo@gregoriofer.com>
2025-11-03 11:59:45 +0100

    3/11/25

---
+++ Programacion JAVA/EntregasJava/src/entregasjava/Tema2/GetCharFromAsciiCode.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entregasjava.Tema2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author goncalda
+ */
+public class GetCharFromAsciiCode {
+    public static void main(String[] args) {
+        Scanner scIn= new Scanner(System.in);
+        System.out.println("Introduce el codigo ascii del caracter que quieres conocer");
+        int valorASCII= scIn.nextInt();
+        char caracter = (char) valorASCII;
+        System.out.printf("El codigo %d corresponde al caracter \"%s\"%n",valorASCII,caracter);
+    }
+}
