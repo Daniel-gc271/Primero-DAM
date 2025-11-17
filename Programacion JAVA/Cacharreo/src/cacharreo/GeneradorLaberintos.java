784e58d7498584a952e0723d06a7dd7e6dc17bcb
Danielgc-271 <daniel.gonzalez.calvo@gregoriofer.com>
2025-11-17 13:16:41 +0100

    17/11/2025

--- Programacion JAVA/Cacharreo/src/cacharreo/GeneradorLaberintos.java
+++ Programacion JAVA/Cacharreo/src/cacharreo/GeneradorLaberintos.java
@@ -4,35 +4,85 @@
  */
 package cacharreo;
 
+import java.util.Random;
+
 /**
  *
  * @author Daniel González
  */
 public class GeneradorLaberintos {
-    public static void main(String[] args) {
-        int grado = 4;
-        int numFilas = grado*grado+1, numColumnas= grado*grado+1;
-        int[][] laberinto=new int [numFilas][numColumnas];
-        for (int i = 0; i < numFilas; i++) {
-            for (int j = 0; j < numColumnas; j++) {
-                if ((i>1)&&(i<numFilas)&&(i%2==0)&&j!=0&&j<numColumnas-1)
-                {
-                laberinto[i-1][j]=1;
-                if (j==numColumnas-2&&i<numColumnas-1) {laberinto[i][j]=1;}
+
+    
+
+    private static char[][] randomizarLaberinto(char[][] laberinto, int iteraciones, Random numAleatorio) {
+        int oI = 0, oJ = 0, contIteraciones = 0, mov = 0;
+        while (contIteraciones <= iteraciones) {
+            for (int i = 0; i < laberinto.length; i++) {
+                for (int j = 0; j < laberinto[0].length; j++) {
+                    if (laberinto[i][j] == 'O') {
+                        oI = i;
+                        oJ = j;
+                        break;
+                    }
+                }
+            }
+            mov = numAleatorio.nextInt(4);
+            if (oJ - 1 >= 0 && mov == 0) {
+                laberinto[oI][oJ] = 'A';
+                laberinto[oI][oJ - 1] = 'O';
+                contIteraciones++;
+            }
+            if (oJ + 1 < laberinto[0].length && mov == 1) {
+                laberinto[oI][oJ + 1] = 'O';
+                laberinto[oI][oJ] = 'D';
+                contIteraciones++;
+            }
+            if (oI - 1 >= 0 && mov == 2) {
+                laberinto[oI][oJ] = 'W';
+                laberinto[oI - 1][oJ] = 'O';
+                contIteraciones++;
+            }
+            if (oI + 1 < laberinto[0].length && mov == 3) {
+                laberinto[oI + 1][oJ] = 'O';
+                laberinto[oI][oJ] = 'S';
+                contIteraciones++;
+            }
+        }
+        return laberinto;
+    }
+
+    private static char[][] generarLaberinto(int grado) {
+        char[][] vectoresUnidos = new char[grado][grado];
+        for (int i = 0; i < vectoresUnidos.length; i++) {
+            for (int j = 0; j < vectoresUnidos[0].length; j++) {
+                if (j != vectoresUnidos[0].length - 1) {
+                    vectoresUnidos[i][j] = 'D';
+                }
+                if (j == vectoresUnidos.length - 1) {
+                    vectoresUnidos[i][j] = 'S';
                 }
             }
         }
-        laberinto[numFilas-2][numColumnas-2]=3;
-        imprimirMatriz(laberinto);
+        vectoresUnidos[vectoresUnidos.length - 1][vectoresUnidos[0].length - 1] = 'O';
+        return vectoresUnidos;
     }
-    private static void imprimirMatriz(int[][] matrix) {
+    private static void imprimirMatriz(char[][] matrix) {
         int numFilas = matrix.length;
         int numColumnas = matrix[0].length;
         for (int i = 0; i < numFilas; i++) {
             for (int j = 0; j < numColumnas; j++) {
-                System.out.printf(" %d", matrix[i][j]);
+                System.out.printf(" %s", matrix[i][j]);
             }
             System.out.println("");
         }
     }
+
+    public static void main(String[] args) {
+        Random numAleatorio = new Random();
+        char[][] laberinto = generarLaberinto(16);
+        laberinto = randomizarLaberinto(laberinto, 4092, numAleatorio);
+        imprimirMatriz(laberinto);
+
+    }
+
 }
--- Programacion JAVA/Cacharreo/src/cacharreo/LaberintoFloodFill.java
+++ Programacion JAVA/Cacharreo/src/cacharreo/LaberintoFloodFill.java
@@ -5,30 +5,9 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.Timer;
 
-public class LaberintoFloodFill extends JFrame {
+public class LaberintoFloodFill  {
 
     public static void main(String[] args) {
-        int celdas = 4;
-        int[][] matrix = new int[celdas * celdas + 1][celdas * celdas];
-        for (int i = 0; i < celdas * celdas + 1; i++) {
-            for (int j = 0; j < celdas * celdas; j++) {
-                if (i % 2 == 0 && j < celdas * celdas - 1) {
-                    matrix[i][j] = 5;
-                } else if (i < celdas * celdas) {
-                    matrix[i][celdas * celdas - 1] = 8;
-                } else {}
-
-            }
-            //5 derecha, 8 abajo
-        }
-        matrix[celdas * celdas][celdas * celdas - 1] = 1;
-        int numFilas = matrix.length;
-        int numColumnas = matrix[0].length;
-        for (int i = 0; i < numFilas; i++) {
-            for (int j = 0; j < numColumnas; j++) {
-                System.out.printf(" %d", matrix[i][j]);
-            }
-            System.out.println("");
-        }
+        
     }
 }
