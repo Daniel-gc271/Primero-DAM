91f0ce455f43697848f324491cc158f99317ca5c
Danielgc-271 <daniel.gonzalez.calvo@gregoriofer.com>
2025-12-05 12:05:21 +0100

    05/12/2025

---
+++ Programacion JAVA/Tema5/src/Ejercicios/AppPolaresACartesianas.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+import 
+/**
+ *
+ * @author goncalda
+ */
+public class AppPolaresACartesianas {
+    public static void main(String[] args) {
+        
+        {
+            Math.sin(0)
+            
+        
+        }
+        
+        
+    }
+    private static void menu() {
+    //Estatico porque no depende de ningun estado de ningun otro objeto o instancia
+    //Es un metodo de sporte que sirve para mostrar el menu
+        System.out.printf("Convertidor de coordenadas polares al sistema cartesiano"
+                + "Instrucciones:"
+                + "\tDebes introducir tu angulo en grados"
+                + "\tLa distancia no debe ser negativa"
+                + "Opciones:"
+                + "\t1) Pasar de coordenadas polares a cartesianas"
+                + "\t2) Pasar de coordenadas cartesianes a polares"
+                + "\t3) Salir");
+    
+    }
+    
+    
+}
---
+++ Programacion JAVA/Tema5/src/Ejercicios/UtilidadesPolaresACartesianas.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author goncalda
+ */
+public class UtilidadesPolaresACartesianas {
+    public double degreeToRadian (double grados) {
+    return (grados%360)*180/Math.PI;
+    }
+    public double calcularCoordenadaX(double radio, double angulo) {
+    return Math.cos(angulo)*radio;
+    }
+    public double calcularCoordenadaY(double radio, double angulo) {
+    return Math.sin(angulo)*radio;
+    public double validarPositivo() {
+        Scanner scIn = new Scanner(System.in);
+        try
+            
+        
+        
+        
+    }
+    }
+    
+    
+}
